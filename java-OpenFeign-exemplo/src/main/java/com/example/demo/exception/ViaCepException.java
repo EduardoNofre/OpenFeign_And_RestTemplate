45ae95a4da691a657a4da68c658658e9b7eebd33
eduardo <nofrereis@gmail.com>
2020-01-16 01:22:29 -0300

    primeiro

---
+++ java-OpenFeign-exemplo/src/main/java/com/example/demo/exception/ViaCepException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class ViaCepException extends RuntimeException {
+
+	public ViaCepException(String message) {
+		super(message);
+	}
+
+}
---
+++ java-OpenFeign-exemplo/src/main/java/com/example/demo/model/CepModel.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CepModel {
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String unidade;
+	private String ibge;
+	private String gia;
+
+}
